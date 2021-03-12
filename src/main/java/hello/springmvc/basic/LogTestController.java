7237c30458926e7c0e1b3c3fef2222e09bde1e96
kim <rkdlem48@gmail.com>
2021-03-13 00:20:44 +0900

    Initial commit

---
+++ src/main/java/hello/springmvc/basic/LogTestController.java
@@ -0,0 +1,25 @@
+package hello.springmvc.basic;
+
+
+import lombok.extern.slf4j.Slf4j;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@Slf4j
+public class LogTestController {
+
+    @RequestMapping("log-test")
+    public String logTest(){
+        String name = "Spring";
+        log.trace("trace log={}", name);
+        log.debug("trace log={}", name);
+        log.info("info log={}", name);
+        log.warn("trace log={}", name);
+        log.error("trace log={}", name);
+
+        return "ok";
+    }
+}
